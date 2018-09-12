e6add89b1cfbcdf3dddc166614b1d8e59452b50b
David Scott Perez <david.scott.perez@live.com>
2018-09-11 21:54:37 -0700

    some really awesome jquery and js functions

--- littlebloom.js
+++ littlebloom.js
@@ -10,3 +10,29 @@
      });
 
    
+//floating scroll to top botton 
+//this automatically hides on document load
+     $(document).ready(function() {
+        $('.fixed-action-btn').hide();
+     });
+
+     //once you've scrolled 600px, the div will fade in, if you scroll less than 600px it will fade again
+     $(window).scroll(function() {
+         if ($(this).scrollTop() < 600)
+            {
+                $('.fixed-action-btn').fadeOut();
+            } else {
+                $('.fixed-action-btn').fadeIn();
+            }
+     });
+
+    //  $("#go-to-albums").click(function() {
+    //    scrollTo(window, ".albums");
+    //     });
+
+    const toAlbums = () => {
+        window.scrollTo({
+            top: 730,
+            behavior: "smooth"
+        });
+    }
